73f0775d31a858292a5c3dfa96b966ebe05fcf32
Chris <cl2871@nyu.edu>
2020-02-09 12:39:25 -0800

    Added logic to toggle sidebar on mobile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tailwind-Angular-Demo';
+
+  @ViewChild('navbarMenu', {static: false}) navbarMenu: ElementRef;
+
+  /**
+   * On mobile, the hamburger is used to toggle the sidebar.
+   * On mobile, the sidebar when opened will overlay on top of the messages view.
+   */
+  toggleNavbar() {
+    this.navbarMenu.nativeElement.classList.toggle('hidden');
+    this.navbarMenu.nativeElement.classList.toggle('absolute');
+  }
 }
